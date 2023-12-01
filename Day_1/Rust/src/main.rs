1ee9756a31444ee1ef471d4e24e10b378a0ebbc7
hua-en <huaen.lim@gmail.com>
2023-12-01 13:55:44 +0800

    AOC 1.1

--- Day_1/Rust/src/main.rs
+++ Day_1/Rust/src/main.rs
@@ -1,3 +1,35 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let tot_val = naive_count("input.txt");
+    println!("{}", tot_val);
+}
+
+fn naive_count(path: &str) -> i32 {
+    let fullstr = read_to_string(path).unwrap();
+    let lines = fullstr.lines();
+
+    let mut tot_val = 0;
+
+    for line in lines {
+        // Find first and last digit
+        let mut fst = ' ';
+        let mut lst = ' ';
+        let mut found_fst = false;
+        for c in line.chars() {
+            if !c.is_ascii_digit() {
+                continue;
+            }
+            if !found_fst {
+                fst = c;
+                found_fst = true;
+            }
+            lst = c;
+        }
+        // Combine them together and add
+        let fin_val = format!("{}{}", fst, lst).parse::<i32>().unwrap();
+        tot_val += fin_val;
+    }
+
+    tot_val
 }
